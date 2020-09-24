0b8eadf4b8efee1d1d4dee7543714247264d34d8
GertrudaSK <penkta@gmail.com>
2020-09-24 14:48:11 +0300

    Python Programs

--- Projects-Odoo/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import project
-from . import invoice
-from . import works
-from . import hr_employee_inherited
-from . import partner_inherited
-from . import wizard
--- Projects-Odoo/models/partner_inherited.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    projektas_ids = fields.One2many('projektai.project', 'klientas_id',
-                                    string="Project", readonly='True')
